7d518e5f32110b8114be3e67b60192b2e2c0c922
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-03-20 16:57:05 +0100

    [UPD] Ex1 DONE

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // ESERCIZIO UNO - Sezione mail random
 
 console.log('Mail');
-const mail = ['GigioBello@gmail.com', 'MarcoVerdi@hotmail.com', 'GuglielmoPaninaro@yahoo.com', 'GioielliFantastici@outlook.com'];
+const mail = ['GigioBello@gmail.com', 'MarcoVerdi@hotmail.com', 'GuglielmoPaninaro@yahoo.com', 'GioielliFantastici@outlook.com',];
 let userMail;
 let validMail = false;
 
@@ -9,7 +9,7 @@ let validMail = false;
 
 while (!validMail) {
     userMail = prompt('Inserire qui la tua mail');
-    if(userMail.includes('@') && userMail=includes('.')){
+    if (userMail.includes('@') && userMail.includes('.')){
         validMail = true;
     }
     else{
@@ -17,3 +17,20 @@ while (!validMail) {
     }
 }
 let found = false;
+
+//CHECK IF EMAIL IN ARRAY
+
+for (let i = 0; i < mail.length; i++){
+    if (userMail === mail[i]) {
+        found = true;
+    }
+}
+
+//RESULT MESSAGE
+
+if(found){
+    console.log('Questa mail è presente');
+}
+else{
+    console.log('Questa mail NON è presente');
+}
